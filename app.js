c706506b295fb930027fab61ba1b8b289d67fc76
Rod <webhead.rsm.jsm@gmail.com>
2025-04-27 21:34:48 +0800

    Added Port

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const cors = require("cors");
 require("dotenv/config");
 
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 8080;
 const allowedOrigins = [
   "*",
   "http://localhost:5000",
